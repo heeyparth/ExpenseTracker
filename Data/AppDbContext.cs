b469e21828d3d6761a2f224e1c8bd28bbcfe4953
parth <parth.shrivastava805@gmail.com>
2025-07-01 00:31:08 +0530

    nothing just pussh

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ExpenseTrackerApp.Models;
+using System.Collections.Generic;
+
+namespace ExpenseTrackerApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Expenses> Expenses { get; set; }
+    }
+}
---
+++ Models/Expenses.cs
@@ -0,0 +1,37 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace ExpenseTrackerApp.Models
+{
+    public enum Category
+    {
+        Food,
+        Travel,
+        Electricity,
+        Personal,
+        Others
+    }
+
+    public class Expenses
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(20)") ]
+        public required string Title { get; set; }
+
+        [Required]
+        
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateOnly Date { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+    }
+}
